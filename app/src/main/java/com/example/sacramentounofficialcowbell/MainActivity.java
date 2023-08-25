c480c454a77dbc38648acb8ab6bbc2c1ce0246bf
shynalprasad <shynal_007@yahoo.com>
2023-08-25 02:24:52 -0700

    button played sound when click

--- app/src/main/java/com/example/sacramentounofficialcowbell/MainActivity.java
+++ app/src/main/java/com/example/sacramentounofficialcowbell/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.sacramentounofficialcowbell;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton imageButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageButton = findViewById(R.id.imageButton);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.cowbell);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
     }
 }
