e9f5f8c6b0e6e04a399d0c5015a83466330d4c60
shynalprasad <shynal_007@yahoo.com>
2023-08-26 04:06:46 -0700

    Added Shake detector

--- app/src/main/java/com/example/sacramentounofficialcowbell/MainActivity.java
+++ app/src/main/java/com/example/sacramentounofficialcowbell/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.sacramentounofficialcowbell;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton imageButton;
+    private ImageButton imageButton;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
+
 
 
 
@@ -25,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer.start();
             }
         });
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector(new ShakeDetector.OnShakeListener(){
+            public void onShake(){
+                mediaPlayer.start();
+            }
+        });
+
+
 
     }
 }
